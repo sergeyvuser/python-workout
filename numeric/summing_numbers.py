f827da00e7da7f29ac00c593b73769a5d4c2bb92
sergeyv <sergeyv@genustechnologies.com>
2026-02-04 09:30:01 +0300

    refactor(core): change class Sum in the summing_numbers.py

--- numeric/summing_numbers.py
+++ numeric/summing_numbers.py
@@ -1,24 +1,34 @@
-class Sum:
-    def mysum(*numbers, start: int = 0) -> int:
+class Numbers:
+    """Numbers class with summing function"""
+
+    def __init__(self, *numbers):
+        """
+        Args:
+            *numbers: numbers to sum
+        """
+        self.numbers = numbers
+
+    def mysum(self, start: int = 0) -> int:
         """
         Summing function with start parameter
 
         Args:
-            *numbers: numbers to sum
             start: start value
         Returns:
             sum of numbers + start
         """
         summ = start
-        for number in numbers:
+        for number in self.numbers:
             summ += number
         return summ
 
 
 if __name__ == "__main__":
-    sum_test = Sum()
-    print(sum_test.mysum(10, 20, 30, 40))
-    print(sum_test.mysum(10, 20, 30, 40, start=10))
-    print(sum_test.mysum(*[1, 2, 3, 4, 5], start=0))
-    print(sum_test.mysum(*range(1, 101)))
-    print(sum_test.mysum(*range(1, 101), start=10))
+    sum_test1 = Numbers(10, 20, 30, 40)
+    sum_test2 = Numbers(*[1, 2, 3, 4, 5])
+    sum_test3 = Numbers(*range(1, 101))
+    print(sum_test1.mysum())
+    print(sum_test1.mysum(10))
+    print(sum_test2.mysum(0))
+    print(sum_test3.mysum())
+    print(sum_test3.mysum(10))
